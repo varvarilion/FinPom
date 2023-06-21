a4add3246d704e0fb9bdea47fea434f4d4bafdb9
varvarilion <137167742+varvarilion@users.noreply.github.com>
2023-06-21 12:57:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Hello World')
+        for row in range(1, 3361):
+            table_name = str(sheet[row][0].value)
+            table_name = table_name.lower()
+            if mes_name in table_name:
+                length += len(sheet[row][0].value)
+                line = str(n) + '. ' + sheet[row][0].value
+                n += 1
