8bbab1f934855d8746ccc9b96557d9afb83643a7
varvarilion <137167742+varvarilion@users.noreply.github.com>
2023-06-21 15:54:46 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,3 +19,29 @@ def ready_for_answer(message):
 def func_to_use_next_in_code(data):
     # do stuff..
     print(data)
+
+
+
+
+
+from openpyxl import *
+
+wb = load_workbook('balances.xlsx')
+sheet = wb.active
+
+
+def excel():
+    sheet.column_dimensions['A'].width = 30
+    sheet.column_dimensions['B'].width = 10
+
+
+current_row = sheet.max_row
+current_column = sheet.max_column
+
+
+sheet.cell(row=current_row + 1, column=1).value = input()
+sheet.cell(row=current_row + 1, column=2).value = input()
+wb.save('balances.xlsx')
+if __name__ == "__main__":
+    excel()
+wb.save('balances.xlsx')
