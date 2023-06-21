301a3c610a52703cac11cc05412a06dd3be90d3b
Artiss08 <137165844+Artiss08@users.noreply.github.com>
2023-06-21 14:39:54 +0300

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,7 +1,40 @@
-import sqlite3
+from openpyxl import Workbook
 
-with sqlite3.connect('bd/register.bd') as bd:
-    cursor = bd.cursor()
-    query = """INSERT INTO expenses (name, password) VALUES(user_name, user_password)   """
-    cursor.execute(query)
-    bd.commit()
+wb = Workbook()
+
+a = 'dsvkjdnvkjdkdnfdnvfd'
+n = '0345'
+
+ws1 = wb.create_sheet("reg", 0)
+
+
+
+def vhod():
+    for row in range(1, 3361):
+        table_name = str(ws1[row][0].value)
+        table_password = str(ws1[row][1].value)
+        if a == table_name and n == table_password:
+            print()
+        else:
+            print("Вы зарегистрированы!")
+#           \/
+#          вход
+
+def register():
+    for row in range(1, 3361):
+        table_name = str(ws1[row][0].value)
+        if a == table_name:
+            print("Такой логи уже есть")
+        else:
+            print("Вы зарегистрированы!")
+#           \/
+#       регистрация
+
+
+
+
+
+ws2 = wb.create_sheet("ras", 1)
+ws3 = wb.create_sheet("doh", 2)
+
+wb.save('FinPom.xlsx')
