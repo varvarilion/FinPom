e21eaf9f975a3909cd89eeab999b61bb344e5995
varvarilion <137167742+varvarilion@users.noreply.github.com>
2023-06-21 15:54:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
-        for row in range(1, 3361):
-            table_name = str(sheet[row][0].value)
-            table_name = table_name.lower()
-            if mes_name in table_name:
-                length += len(sheet[row][0].value)
-                line = str(n) + '. ' + sheet[row][0].value
-                n += 1
+@bot.message_handler(state=MyStates.age, is_digit=True)
+def ready_for_answer(message):
+    """
+    State 3. Will process when user's state is MyStates.age.
+    """
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        msg = ("Ready, take a look:\n<b>"
+               f"Name: {data['name']}\n"
+               f"Surname: {data['surname']}\n"
+               f"Age: {message.text}</b>")
+
+        # here is this function
+        func_to_use_next_in_code(data)
+
+        bot.send_message(message.chat.id, msg, parse_mode="html")
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+def func_to_use_next_in_code(data):
+    # do stuff..
+    print(data)
