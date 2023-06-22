21dd3b661e0eac6c3abd4c78bc93f920979a18eb
Joetuoy <118456099+Joetuoy@users.noreply.github.com>
2023-06-22 14:27:10 +0300

    Add files via upload

--- 1.py
+++ 1.py
@@ -1,49 +1,16 @@
 from openpyxl import *
-import datetime
 
 wb = load_workbook('FinPom2.xlsx')
 
-sheet2 = wb.worksheets[1]
-name = ""
-nal = 100
-beznal = 500
-nal2 = 500
-beznal2 = 100
-
-
-def doch1():
-    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
-    sheet2.cell(row=current_row + 1, column=1).value = name
-    sheet2.cell(row=current_row + 1, column=3).value = "Наличныe"
-    sheet2.cell(row=current_row + 1, column=2).value = nal
-    sheet2.cell(row=current_row + 1, column=4).value = "Доход"
-
-
-def doch2():
-    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
-    sheet2.cell(row=current_row + 1, column=1).value = name
-    sheet2.cell(row=current_row + 1, column=3).value = "Безнал"
-    sheet2.cell(row=current_row + 1, column=2).value = beznal
-    sheet2.cell(row=current_row + 1, column=4).value = "Доход"
-
-
-def rash1():
-    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
-    sheet2.cell(row=current_row + 1, column=1).value = name
-    sheet2.cell(row=current_row + 1, column=3).value = "Наличныe"
-    sheet2.cell(row=current_row + 1, column=2).value = nal2
-    sheet2.cell(row=current_row + 1, column=4).value = "Расход"
-
-
-def rash2():
-    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
-    sheet2.cell(row=current_row + 1, column=1).value = name
-    sheet2.cell(row=current_row + 1, column=3).value = "Безнал"
-    sheet2.cell(row=current_row + 1, column=2).value = beznal2
-    sheet2.cell(row=current_row + 1, column=4).value = "Расход"
-
+sheet2 = wb.worksheets[0]
 current_row = sheet2.max_row
 
-doch1()
+sheet2.cell(row=current_row + 1, column=8).value = int(input())  # доход наличные
+
+sheet2.cell(row=current_row + 1, column=9).value = int(input())  # доход безнал
+
+sheet2.cell(row=current_row + 1, column=5).value = int(input())  # расход наличные
+
+sheet2.cell(row=current_row + 1, column=6).value = int(input())  # расход безнал
 
 wb.save('FinPom2.xlsx')
