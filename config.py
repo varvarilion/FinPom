3f2668b21fbe30d6920692b48066333547fd3611
Joetuoy <118456099+Joetuoy@users.noreply.github.com>
2023-06-22 12:49:54 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,92 +1,49 @@
 from openpyxl import *
+import datetime
+
+wb = load_workbook('FinPom2.xlsx')
+
+sheet2 = wb.worksheets[1]
+name = "ff"
+nal = 100
+beznal = 500
+nal2 = 500
+beznal2 = 100
 
 
-wb = load_workbook('FinPom.xlsx')
-sheet1 = wb.active
+def doch1():
+    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
+    sheet2.cell(row=current_row + 1, column=1).value = name
+    sheet2.cell(row=current_row + 1, column=3).value = "Наличныe"
+    sheet2.cell(row=current_row + 1, column=2).value = nal
+    sheet2.cell(row=current_row + 1, column=4).value = "Доход"
 
 
-def excel():
-    sheet1.column_dimensions['A'].width = 20
-    sheet1.column_dimensions['B'].width = 20
-    sheet1.column_dimensions['C'].width = 20
-    sheet1.column_dimensions['D'].width = 20
-    sheet1.column_dimensions['E'].width = 20
-    sheet1.column_dimensions['F'].width = 20
-    sheet1.column_dimensions['G'].width = 20
-    sheet1.column_dimensions['H'].width = 20
-    sheet1.column_dimensions['I'].width = 20
-    sheet1.column_dimensions['J'].width = 20
-    sheet1.column_dimensions['K'].width = 20
-    sheet1.column_dimensions['L'].width = 20
-    sheet1.column_dimensions['M'].width = 20
-    sheet1.column_dimensions['N'].width = 20
-    sheet1.column_dimensions['O'].width = 20
-    sheet1.column_dimensions['P'].width = 20
-    sheet1.column_dimensions['Q'].width = 20
+def doch2():
+    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
+    sheet2.cell(row=current_row + 1, column=1).value = name
+    sheet2.cell(row=current_row + 1, column=3).value = "Безнал"
+    sheet2.cell(row=current_row + 1, column=2).value = beznal
+    sheet2.cell(row=current_row + 1, column=4).value = "Доход"
 
 
-def wordfinder(ABC):
-    for i in range(1, sheet1.max_row + 1):
-        for j in range(1, sheet1.max_column + 1):
-            if ABC == sheet1.cell(i, j).value:
-                a = i
-                return a
+def rash1():
+    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
+    sheet2.cell(row=current_row + 1, column=1).value = name
+    sheet2.cell(row=current_row + 1, column=3).value = "Наличныe"
+    sheet2.cell(row=current_row + 1, column=2).value = nal2
+    sheet2.cell(row=current_row + 1, column=4).value = "Расход"
 
 
-current_row = sheet1.max_row
+def rash2():
+    sheet2.cell(row=current_row + 1, column=5).value = datetime.date.today()
+    sheet2.cell(row=current_row + 1, column=1).value = name
+    sheet2.cell(row=current_row + 1, column=3).value = "Безнал"
+    sheet2.cell(row=current_row + 1, column=2).value = beznal2
+    sheet2.cell(row=current_row + 1, column=4).value = "Расход"
 
-if wordfinder(str(input("ID"))) == None:
-    sheet1.cell(row=current_row + 1, column=1).value = input("ID")
-    doh = str(input("Категория:"))
-    if doh == "Соц.выплаты":
-        sheet1.cell(row=current_row + 1, column=14).value = input("Доход:")
-    elif doh == "зарплата":
-        sheet1.cell(row=current_row + 1, column=15).value = input("Доход:")
-    elif doh == "карманые деньги":
-        sheet1.cell(row=current_row + 1, column=16).value = input("Доход:")
-    else:
-        sheet1.cell(row=current_row + 1, column=17).value = input("Доход:")
-    ras = str(input("Категория:"))
-    if ras == "развлечение":
-        sheet1.cell(row=current_row + 1, column=6).value = input("Доход:")
-    elif ras == "услуги":
-        sheet1.cell(row=current_row + 1, column=7).value = input("Доход:")
-    elif ras == "питание":
-        sheet1.cell(row=current_row + 1, column=8).value = input("Доход:")
-    elif ras == "медицина":
-        sheet1.cell(row=current_row + 1, column=9).value = input("Доход:")
-    elif ras == "транспорт":
-        sheet1.cell(row=current_row + 1, column=10).value = input("Доход:")
-    elif ras == "товары":
-        sheet1.cell(row=current_row + 1, column=11).value = input("Доход:")
-    else:
-        sheet1.cell(row=current_row + 1, column=12).value = input("Доход:")
+current_row = sheet2.max_row
 
-else:
-    a = wordfinder(str(input("ID")))
-    doh = str(input("Категория:"))
-    if doh == "Соц.выплаты":
-        sheet1.cell(row=a, column=14).value = input("Доход:")
-    elif doh == "зарплата":
-        sheet1.cell(row=a, column=15).value = input("Доход:")
-    elif doh == "карманые деньги":
-        sheet1.cell(row=a, column=16).value = input("Доход:")
-    else:
-        sheet1.cell(row=a, column=17).value = input("Доход:")
-    ras = str(input("Категория:"))
-    if ras == "развлечение":
-        sheet1.cell(row=a, column=6).value = input("Доход:")
-    elif ras == "услуги":
-        sheet1.cell(row=a, column=7).value = input("Доход:")
-    elif ras == "питание":
-        sheet1.cell(row=a, column=8).value = input("Доход:")
-    elif ras == "медицина":
-        sheet1.cell(row=a, column=9).value = input("Доход:")
-    elif ras == "транспорт":
-        sheet1.cell(row=a, column=10).value = input("Доход:")
-    elif ras == "товары":
-        sheet1.cell(row=a, column=11).value = input("Доход:")
-    else:
-        sheet1.cell(row=a, column=12).value = input("Доход:")
-excel()
-wb.save('FinPom.xlsx')
+doch1()
+
+wb.save('FinPom2.xlsx')
